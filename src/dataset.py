700b7135e34630ef8c82901f775befb2266e36c0
arvindajaybharadwaj <arvindajayarts@gmail.com>
2025-12-01 23:34:35 +0530

    Create collate_fn to pad samples of a batch to same length

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
 
 class CustomEuroParlDataset(Dataset):
     def __init__(self, enc_in, dec_in, dec_tgt):
@@ -15,4 +16,17 @@ class CustomEuroParlDataset(Dataset):
         dec_input = torch.tensor(self.dec_inputs[index], dtype=torch.int64)
         dec_target = torch.tensor(self.dec_targets[index], dtype=torch.int64)
         return (enc_input, dec_input, dec_target)
-    
+
+def collate_fn(batch):
+    enc_seqs = [item[0] for item in batch]
+    dec_in_seqs = [item[1] for item in batch]
+    dec_tgt_seqs = [item[2] for item in batch]
+
+    enc_lens = torch.tensor([len(seq) for seq in enc_seqs], dtype=torch.int64)
+    dec_lens = torch.tensor([len(seq) for seq in dec_in_seqs], dtype=torch.int64)
+
+    padded_enc = pad_sequence(enc_seqs, batch_first=True, padding_value=0)
+    padded_dec_in = pad_sequence(dec_in_seqs, batch_first=True, padding_value=0)
+    padded_dec_tgt = pad_sequence(dec_tgt_seqs, batch_first=True, padding_value=0)
+
+    return padded_enc, padded_dec_in, padded_dec_tgt, enc_lens, dec_lens
