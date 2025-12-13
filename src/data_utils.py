e944b38c09dbaa961d379504cd12e733fb8a4d60
arvindajaybharadwaj <arvindajayarts@gmail.com>
2025-12-13 22:12:28 +0530

    Implement dataset splitting and DataLoader pipeline

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,5 +1,8 @@
 import torch 
 from sklearn.model_selection import train_test_split
+from src.dataset import CustomEuroParlDataset
+from src.dataset import collate_fn
+from torch.utils.data import DataLoader
 
 def load_processed_data():
     enc_in = torch.load("../data/processed/enc_in.pt")
@@ -17,4 +20,26 @@ def split_data(enc_in, dec_in, dec_tgt, train_ratio=0.9, val_ratio=0.05, random_
 
     return (train_enc_in, val_enc_in, test_enc_in, train_dec_in, val_dec_in, test_dec_in, train_dec_tgt, val_dec_tgt, test_dec_tgt)
 
+def get_dataloaders(batch_size, train_ratio=0.9, val_ratio=0.05, random_state=42):
+    # loading the dataset
+    enc_in, dec_in, dec_tgt = load_processed_data()
 
+    #splitting data
+    split_dataset = split_data(enc_in, dec_in, dec_tgt, train_ratio=train_ratio, val_ratio=val_ratio, random_state=random_state)
+
+    train_enc_in, val_enc_in, test_enc_in = split_dataset[0], split_dataset[1], split_dataset[2]
+    train_dec_in, val_dec_in, test_dec_in = split_dataset[3], split_dataset[4], split_dataset[5]
+    train_dec_tgt, val_dec_tgt, test_dec_tgt = split_dataset[6], split_dataset[7], split_dataset[8]
+
+    # creating the datasets
+    train_dataset = CustomEuroParlDataset(train_enc_in, train_dec_in, train_dec_tgt)
+    val_dataset = CustomEuroParlDataset(val_enc_in, val_dec_in, val_dec_tgt)
+    test_dataset = CustomEuroParlDataset(test_enc_in, test_dec_in, test_dec_tgt)
+
+    # creating the dataloaders
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
+
+    # returning the loaders
+    return (train_loader, val_loader, test_loader)
